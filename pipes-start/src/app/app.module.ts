cc9d78480779226351705e90cd43b36e5c10dcd2
Anmol Saraswat <anmolsaraswat@outlook.com>
2020-09-18 21:49:58 +0530

    The Project contains basic pipes concept of Angular.

---
+++ pipes-start/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShortenPipe,
+    ReversePipe,
+    SortPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pipes-start/src/app/sort.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+  transform(value: any, propName: string){
+    return value.sort((a,b) => (a[propName] >b[propName] ? 1 : -1)
+    );
+  }
+}
